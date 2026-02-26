ac10366e72015522a8f97fda34d44828cf589692
RoboticsClubMDC <203731947+RoboticsClubMDC@users.noreply.github.com>
2026-02-25 22:50:25 -0500

    Update setup.py

--- Development/ros2/src/qcar2_autonomy/setup.py
+++ Development/ros2/src/qcar2_autonomy/setup.py
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'avoid_sidewalks = autonomy.avoid_sidewalks:main',
             'lane_detector = autonomy.lane_detector:main',
             'lane_seg_node = autonomy.lane_seg_node:main',
             'path_follower = autonomy.nav_to_pose:main',
